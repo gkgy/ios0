7cfb0286536d9608c9939280198eb1de68ac1df7
gkgy（天使的龙蛋） <gkgy@qq.com>
2017-04-27 23:15:32 +0800

    Hello word程序加入

---
+++ ios1/A/A/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  A
+//
+//  Created by 高坤 on 2017/4/27.
+//  Copyright © 2017年 gkgy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ios1/MyPlayground.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//var意思就是variable，变量
+var a = 1 + 4
+
+print(a)
+print(str)
+
+if 4 > 5 {
+    print ("厉害")
+}else
+{
+print ("不行")
+}
+
+
+
